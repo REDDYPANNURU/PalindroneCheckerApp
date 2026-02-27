8a89f9c823f7d31019f3776939c43752f5c72d53
REDDYPANNURU <raj.pannuru@gmil.com>
2026-02-27 09:06:06 +0530

    [Name] Added USECASE RELATED NAME

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,5 +1,6 @@
 public class PalindromeCheckerApp {
     public class void main(String[] args){
         System.out.println("Welcome to Palindrone Checker App Management System");
+        System.out.println("Modify this logic to usecase 2");
     }
 }
